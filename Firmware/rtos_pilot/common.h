1fa95f6696d18207721c8386394fbc8c0af3b193
tom.pycke <tom.pycke@65cda202-c726-11de-a2e8-8d36a3108c74>
2010-06-26 14:05:22 +0000

    Added common.h

---
+++ Firmware/rtos_pilot/common.h
@@ -0,0 +1,8 @@
+#define PI         3.14159
+#define RAD2DEG(x) x*(180.0/3.14159)
+#define DEG2RAD(x) x*(3.14159/180.0)
+#define G          9.81
+
+#define MAX(a,b) (a)>(b)?(a):(b)
+#define MIN(a,b) (a)<(b)?(a):(b)
+
