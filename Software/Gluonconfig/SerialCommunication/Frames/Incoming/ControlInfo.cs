a8dc3b1b9bc49d7e10e1bd906bb15d8ec4a615e7
tom.pycke <tom.pycke@65cda202-c726-11de-a2e8-8d36a3108c74>
2010-10-05 20:00:45 +0000

    (forgot to include a file)

---
+++ Software/Gluonconfig/SerialCommunication/Frames/Incoming/ControlInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Communication.Frames.Incoming
+{
+    public class ControlInfo
+    {
+        public enum FlightModes { 
+            MANUAL = 0, STABILIZED = 1, AUTOPILOT = 2, LOITER = 3, RETURN = 4 
+        };
+
+        public FlightModes FlightMode;
+        public double HeightAboveStartGround;
+        public int CurrentNavigationLine;
+    }
+}
